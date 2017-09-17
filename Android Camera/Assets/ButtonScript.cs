2d8c1463cb771388fd4355c57a7d16b7ab6abb52
Ming Feng Wan <ming.wan@mail.utoronto.ca>
2017-09-17 15:10:59 -0400

    added the code comments

--- Android Camera/Assets/ButtonScript.cs
+++ Android Camera/Assets/ButtonScript.cs
@@ -13,38 +13,55 @@ public class ButtonScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		WebCamDevice[] devices = WebCamTexture.devices;
+		//Opens the front camera in the phone, or back camera in the laptop
 		deviceName = devices[1].name;
 		webcamTexture = new WebCamTexture(deviceName, Screen.width, Screen.height);
+
+		//Get the two game objects
 		image = GameObject.Find ("RawImage").GetComponent<RawImage> ();
 		rgb = GameObject.Find ("Pixels").GetComponent<Text> ();
+
+		//Resize the image
 		image.rectTransform.sizeDelta = new Vector2 (Screen.height, Screen.width);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//set the image into displaying the captured video
 		image.material.mainTexture = webcamTexture;
+
 		if (webcamTexture.isPlaying) {
+			//Get the video raw data
 			data = webcamTexture.GetPixels ();
+
 			double average_r = 0f;
 			double average_g = 0f;
 			double average_b = 0f;
+
+			//Add the rgb value in each pixel
 			foreach (Color color in data) {
 				average_r += color.r;
 				average_g += color.g;
 				average_b += color.b;
 			}
+
+			//Caculate the average
 			average_r = average_r / data.Length;
 			average_g = average_g / data.Length;
 			average_b = average_b / data.Length;
+
 			rgb.text = average_r.ToString ("N1") + " " + average_g.ToString ("N1") + " " + average_b.ToString ("N1");
 		}
 	}
 
+	//Opens the camera and plays the video captured
 	public void Play(){
 		webcamTexture.Play();
 	}
 
+	//Pause the video
 	public void Pause(){
 		webcamTexture.Stop ();
+		rgb.text = "RGB";
 	}
 }
